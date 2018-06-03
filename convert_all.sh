d8716c83f3e2dd297e72511af601d7cb15c70d24
Kotaro Terada <tera@toki.waseda.jp>
2018-06-03 20:07:34 +0900

    Update URL and sitename etc.

--- convert_all.sh
+++ convert_all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-./convert.py markdown/cv.md -o cv.html --lang en --locale en_US
-./convert.py markdown/cv.ja.md -o cv.ja.html --lang ja --locale ja_JP --canonical cv.ja.html
+./convert.py markdown/cv.md -o cv.html --lang en --locale en_US --canonical cv
+./convert.py markdown/cv.ja.md -o cv.ja.html --lang ja --locale ja_JP --canonical cv.ja
